527286b79255935f991b12c77318135bddfd5ab3
Kamil Baldyga <kbaldyga@gmail.com>
2010-03-08 18:01:02 +0100

    intersection check, generating Rectangle list from Element list

--- pcm/pcm/Program.fs
+++ pcm/pcm/Program.fs
@@ -17,17 +17,18 @@ let testBoard : Board =
     let c = Element.Brick
     let s = Element.Path
     in
-    [
-        [ s;s;s;s;s;s;s;s;s;s ] ;
-        [ s;s;c;s;c;s;s;s;s;s ] ;
-        [ s;c;s;s;c;s;s;s;s;s ] ;
-        [ s;c;s;c;c;s;s;s;s;s ] ;
-        [ s;s;s;s;s;s;s;s;c;s ] ;
-        [ s;s;s;s;c;s;s;s;s;s ] ;
-        [ s;s;s;s;c;s;s;s;s;s ] ;
-        [ s;s;s;s;s;c;s;s;s;s ] ;
-        [ s;s;s;s;s;s;s;s;s;s ] ;
-        [ s;s;s;s;s;s;s;s;s;s ] ;
+    [   [ c;c;c;c;c;c;c;c;c;c;c;c ] ;
+        [ c;s;s;s;s;s;s;s;s;s;s;c ] ;
+        [ c;s;s;c;s;c;s;s;s;s;s;c ] ;
+        [ c;s;c;s;s;c;s;s;s;s;s;c ] ;
+        [ c;s;c;s;c;c;s;s;s;s;s;c ] ;
+        [ c;s;s;s;s;s;s;s;s;c;s;c ] ;
+        [ c;s;s;s;s;c;s;s;s;s;s;c ] ;
+        [ c;s;s;s;s;c;s;s;s;s;s;c ] ;
+        [ c;s;s;s;s;s;c;s;s;s;s;c ] ;
+        [ c;s;s;s;s;s;s;s;s;s;s;c ] ;
+        [ c;s;s;s;s;s;s;s;s;s;s;c ] ;
+        [ c;c;c;c;c;c;c;c;c;c;c;c ] ;
     ]
 
 type klocek = 
@@ -40,7 +41,7 @@ type klocek =
     end
 
 type Ludzik(xsize,ysize) =
-    let mutable location = new Rectangle(0,0,xsize,ysize)
+    let mutable location = new Rectangle(30,30,xsize,ysize)
     let mutable speed = 5.0
     member self.changeLocation(x,y) = location <- new Rectangle(self.Location.X+x,self.Location.Y+y,location.Width, location.Height)
     member self.changeSpeed = fun s -> speed <- s 
@@ -100,22 +101,53 @@ let form =
     form.Height <- 500 ;  
     form.Invalidate(); 
     form
+
+let bricksOnBoard board = 
+    let flatten list = 
+        let rec aux list acc=
+            match list with 
+                | [] -> acc
+                | h::t -> aux t (h@acc)
+        in aux list []
+    // that complitated funcion takes list of Elements on row, checks if any of them is Element.Brick
+    // and if yes, appends its x and y coordinate to acc and return that list, so eventualy we've got
+    // y [Element.Brick, Element.Path, Element.Brick] -> [(0,y),(2,y)]
+    let generateRectangles list = 
+        let rec aux list acc = 
+            match list with 
+                | [] -> List.rev acc
+                | h::t -> aux t (new Rectangle(fst h * PileSize, snd h * PileSize, PileSize, PileSize)::acc)
+        in aux list []
+    let getBricksOnRow y rowList =
+        List.fold (fun acc e ->
+            let x = fst acc
+            in
+            match e with
+                | Element.Path -> (x+1, snd acc)
+                | Element.Brick -> (x+1, ((x,y)::snd acc))
+            ) (0,[]) rowList 
+    in
+    // every row is in form of (rowNumber,[list;of;elements;on;row)
+    List.fold (fun acc row -> 
+        let y = fst acc in
+        (y+1),(getBricksOnRow y row |> snd |> List.rev )::(snd acc) ) (0,[]) board |> snd |> flatten |> generateRectangles
     
 //let player = new Ludzik(PileSize, PileSize)
 type direction = Up | Down | Left | Right
 
 let movePlayer (p:Ludzik) (dir:direction) = 
-    let board = testBoard 
+    let board = bricksOnBoard testBoard
     let howMuch = p.Speed |> Convert.ToInt32 
     let checkCollision (p:Rectangle) =
-        let x = Convert.ToInt32( p.X / PileSize )
-        let y = Convert.ToInt32( p.Y / PileSize )
-        let x' = Convert.ToInt32( (p.X+PileSize) / PileSize )
-        let y' = Convert.ToInt32( (p.Y+PileSize) / PileSize )
-        if p.X < 0 || p.Y < 0 then false else
-        if p.X+PileSize > board.Length*PileSize || p.Y+PileSize > board.Length*PileSize then false else
-        if ( board.Item(y).Item(x) = Element.Brick) then false else true
+//        let x = Convert.ToInt32( p.X / PileSize )
+//        let y = Convert.ToInt32( p.Y / PileSize )
+//        let x' = Convert.ToInt32( (p.X+PileSize) / PileSize )
+//        let y' = Convert.ToInt32( (p.Y+PileSize) / PileSize )
+//        if p.X < 0 || p.Y < 0 then false else
+//        if p.X+PileSize > board.Length*PileSize || p.Y+PileSize > board.Length*PileSize then false else
+//        if ( board.Item(y).Item(x) = Element.Brick) then false else true
         //if ( board.Item(y').Item(x') = Element.Brick ) then false else true 
+        List.fold ( fun acc e -> if p.IntersectsWith(e) then true else acc ) false board
     in
     let newLocation = 
         match dir with
@@ -123,7 +155,7 @@ let movePlayer (p:Ludzik) (dir:direction) =
             | Down -> new Rectangle(p.Location.X, p.Location.Y-howMuch,PileSize,PileSize)
             | Left -> new Rectangle(p.Location.X-howMuch, p.Location.Y,PileSize,PileSize)
             | Right -> new Rectangle(p.Location.X+howMuch, p.Location.Y,PileSize,PileSize)
-    if checkCollision newLocation = true then p.Location <- newLocation ;
+    if checkCollision newLocation = false then p.Location <- newLocation ;
     p 
  
 let keyStates = Array.init 300 ( fun i -> false )
@@ -146,29 +178,7 @@ let ticker =
         if keyStates.[Keys.Space.GetHashCode()] = true then Application.Exit()
         form.Invalidate() ; Console.Write("x ") |> ignore )
 
-let bricksOnBoard board = 
-    let flatten list = 
-        let rec aux list acc=
-            match list with 
-                | [] -> acc
-                | h::t -> aux t (h@acc)
-        in aux list []
-    // that complitated funcion takes list of Elements on row, checks if any of them is Element.Brick
-    // and if yes, appends its x and y coordinate to acc and return that list, so eventualy we've got
-    // y [Element.Brick, Element.Path, Element.Brick] -> [(0,y),(2,y)]
-    let getBricksOnRow y rowList =
-        List.fold (fun acc e ->
-            let x = fst acc
-            in
-            match e with
-                | Element.Path -> (x+1, snd acc)
-                | Element.Brick -> (x+1, ((y,x)::snd acc))
-            ) (0,[]) rowList 
-    in
-    // every row is in form of (rowNumber,[list;of;elements;on;row)
-    List.fold (fun acc row -> 
-        let y = fst acc in
-        (y+1),(getBricksOnRow y row |> snd |> List.rev )::(snd acc) ) (0,[]) board |> snd |> flatten
+
             
 
 do ticker
